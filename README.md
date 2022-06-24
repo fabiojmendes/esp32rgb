87b1239f284380041d543c4b46a2b018c43e431d
Fabio Mendes <fabiojmendes@gmail.com>
2022-06-24 16:23:44 -0400

    Polishing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Graph
-https://www.desmos.com/calculator/mpyvqarj4r
+# Color Spectrum Generation
+
+Color generation funcion is based on this [article](http://justinparrtech.com/JustinParr-Tech/spectrum-generating-color-function-using-sine-waves/)
+
+I used slightly different params to generate this [graph](https://www.desmos.com/calculator/mpyvqarj4r)
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,23 @@ use esp_idf_sys as _;
 use led::{RGB8, WS2812RMT};
 
 const MAX: i32 = 1280;
+const SCALE: f64 = 192.0;
+const SHIFT: f64 = 96.0;
+
+fn color_gen(n: f64, m: f64) -> RGB8 {
+    let a = (2.0 * PI * n) / m + PI / 2.0;
+    // r = sin(a) * scale + shift
+    let r = a.sin() * SCALE + SHIFT;
+    let r = r.clamp(0.0, 255.0) as u8;
+    // g = sin(a - 2π/3) * scale + shift
+    let g = (a - 2.0 * PI / 3.0).sin() * SCALE + SHIFT;
+    let g = g.clamp(0.0, 255.0) as u8;
+    // b = sin(a - 4π/3) * scale + shift
+    let b = (a - 4.0 * PI / 3.0).sin() * SCALE + SHIFT;
+    let b = b.clamp(0.0, 255.0) as u8;
+
+    RGB8 { r, g, b }
+}
 
 fn main() -> anyhow::Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
@@ -15,22 +32,7 @@ fn main() -> anyhow::Result<()> {
     let mut rgb_led = WS2812RMT::new()?;
     loop {
         for n in 0..MAX {
-            let n = n as f64;
-            let m = MAX as f64;
-            let scale = 192.0;
-            let shift = 96.0;
-            let a = (2.0 * PI * n) / m + PI / 2.0;
-            // r = sin(a) * scale + shift
-            let r = a.sin() * scale + shift;
-            let r = r.clamp(0.0, 255.0) as u8;
-            // g = sin(a - 2π/3) * scale + shift
-            let g = (a - 2.0 * PI / 3.0).sin() * scale + shift;
-            let g = g.clamp(0.0, 255.0) as u8;
-            // b = sin(a - 4π/3) * scale + shift
-            let b = (a - 4.0 * PI / 3.0).sin() * scale + shift;
-            let b = b.clamp(0.0, 255.0) as u8;
-
-            let color = RGB8 { r, g, b };
+            let color = color_gen(n as f64, MAX as f64);
             rgb_led.set_pixel(color)?;
             println!("Color: {color}");
             thread::sleep(Duration::from_millis(50));
